c8f7e7d72b514bccfe6f7f209082953bfda41a7b
SUNIL SURENDRAN <133184647+SunilSurendran1906@users.noreply.github.com>
2023-12-11 13:43:33 +0530

    first commit

---
+++ Routes/MessageRoute.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { addMessage, getMessage } from "../Controllers/MessageContoller.js";
+
+const router = express.Router();
+
+router.post("/", addMessage);
+router.get("/:chatId", getMessage);
+
+export default router;
